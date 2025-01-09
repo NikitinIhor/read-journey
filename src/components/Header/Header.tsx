753ab3d1a58e1498ad639b0d04b75df964a48b57
Nikitin <nikitinigor0907@gmail.com>
2025-01-09 19:00:13 +0100

    added burger menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import sprite from "../../assets/icons/sprite.svg";
 import logo from "../../assets/images/Logo.png";
 import Wrapper from "../Wrapper/Wrapper";
 import css from "./Header.module.css";
@@ -5,14 +8,101 @@ import css from "./Header.module.css";
 interface HeaderProps {}
 
 const Header: React.FC<HeaderProps> = () => {
+  const [menu, setMenu] = useState(false);
+
+  const openMenu = () => {
+    setMenu(true);
+  };
+  const closeMenu = () => {
+    setMenu(false);
+  };
+
   return (
     <Wrapper>
       <div className={css.container}>
-        <div className={css.logo}>
+        <Link to="/recommended" className={css.logo}>
           <img src={logo} alt="image of logo" />
           <span>read journey</span>
+        </Link>
+        <nav className={css.nav}>
+          <ul className={css.list}>
+            <li>
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? css.isActive : css.link
+                }
+                to="/recommended"
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? css.isActive : css.link
+                }
+                to="/library"
+              >
+                My library
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+        <div className={css.info}>
+          <div className={css.user}>
+            <div className={css.user_icon}>
+              <span>I</span>
+            </div>
+            <span className={css.user_name}>Ilona Ratushniak</span>
+          </div>
+          <button className={css.burger} type="button">
+            <svg className={css.menu} width={28} height={28}>
+              <use href={`${sprite}#icon-menu`}></use>
+            </svg>
+          </button>
+          <button onClick={openMenu} className={css.logout}>
+            Log out
+          </button>
         </div>
       </div>
+      {menu && (
+        <div className={css.burger_wrapper}>
+          <div className={css.burger_body}>
+            <button className={css.burger_close} type="button">
+              <svg className={css.menu_close} width={28} height={28}>
+                <use href={`${sprite}#icon-x`}></use>
+              </svg>
+            </button>
+            <nav className={css.menu_nav}>
+              <ul className={css.menu_list}>
+                <li>
+                  <NavLink
+                    className={({ isActive }) =>
+                      isActive ? css.isActive : css.link
+                    }
+                    to="/recommended"
+                  >
+                    Home
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    className={({ isActive }) =>
+                      isActive ? css.isActive : css.link
+                    }
+                    to="/library"
+                  >
+                    My library
+                  </NavLink>
+                </li>
+              </ul>
+            </nav>
+            <button onClick={closeMenu} className={css.menu_logout}>
+              Log out
+            </button>
+          </div>
+        </div>
+      )}
     </Wrapper>
   );
 };
--- src/pages/LibraryPage/LibraryPage.tsx
+++ src/pages/LibraryPage/LibraryPage.tsx
@@ -1,9 +1,16 @@
+import Header from "../../components/Header/Header";
 import css from "./LibraryPage.module.css";
 
 interface LibraryPageProps {}
 
 const LibraryPage: React.FC<LibraryPageProps> = () => {
-  return <div className={css.container}> LibraryPage</div>;
+  return (
+    <div className={css.container}>
+      <div className="container">
+        <Header />
+      </div>
+    </div>
+  );
 };
 
 export default LibraryPage;
