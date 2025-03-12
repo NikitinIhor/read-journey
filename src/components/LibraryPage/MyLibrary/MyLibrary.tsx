b4d0734603088af2b4fdacea3084a6e9dad8bfb5
Nikitin <nikitinigor0907@gmail.com>
2025-03-12 13:15:26 +0100

    feat:library input and gallery

--- src/components/LibraryPage/MyLibrary/MyLibrary.tsx
+++ src/components/LibraryPage/MyLibrary/MyLibrary.tsx
@@ -1,9 +1,49 @@
+import { useState } from "react";
+import { RiArrowDownSLine } from "react-icons/ri";
+import books from "../../../assets/images/books.png";
 import css from "./MyLibrary.module.css";
 
-interface MyLibraryProps {}
+const MyLibrary: React.FC = () => {
+  const [selectedValue, setSelectedValue] = useState("all");
 
-const MyLibrary: React.FC<MyLibraryProps> = () => {
-  return <div className={css.container}> fuck</div>;
+  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedValue(event.target.value);
+  };
+
+  return (
+    <div className={css.container}>
+      <div className={css.header}>
+        <h2 className={css.title}>My library</h2>
+
+        <div className={css.selectWrapper}>
+          <select
+            className={css.select}
+            value={selectedValue}
+            onChange={handleSelectChange}
+          >
+            <option value="unread">Unread</option>
+            <option value="progress">In progress</option>
+            <option value="done">Done</option>
+            <option value="all">All books</option>
+          </select>
+
+          <div className={css.icon}>
+            <RiArrowDownSLine color="white" />
+          </div>
+        </div>
+      </div>
+
+      <div className={css.empty}>
+        <div className={css.image}>
+          <img src={books} alt="image of books" />
+        </div>
+        <p className={css.text}>
+          To start training, add <span>some of your books</span> or from the
+          recommended ones.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default MyLibrary;
